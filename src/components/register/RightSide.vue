1597b05c7f0f3a2fc2626c0f8ab115f0fa598882
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-21 00:31:06 +0100

    changes

--- src/components/register/RightSide.vue
+++ src/components/register/RightSide.vue
@@ -156,6 +156,19 @@ input[type="submit"] {
   font-size: 16px;
   line-height: 22px;
   text-align: center;
+}
 
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
