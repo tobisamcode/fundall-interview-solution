4b35dda709d193f94d5c69d96af08cb8d8ccf05a
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-21 00:30:56 +0100

    changes

--- src/components/login/RightSide.vue
+++ src/components/login/RightSide.vue
@@ -8,14 +8,14 @@
 
       <form action="" class="mb-12" @submit="login" >
         <transition name="slide-fade">
-        <div v-if="errorMsg" class="flex items-center justify-between mb-3 py-3 px-5 bg-red-400 text-white rounded">
-          {{ errorMsg }}
-          <span @click="errorMsg = ''" class=" w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover: bg-[rgba(0,0,0,.2)] ">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </span>
-        </div>
+          <div v-if="errorMsg" class="flex items-center justify-between mb-3 py-3 px-5 bg-red-400 text-white rounded">
+            {{ errorMsg }}
+            <span @click="errorMsg = ''" class=" w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover: bg-[rgba(0,0,0,.2)] ">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </span>
+          </div>
         </transition>
         <div class="mb-7 input flex flex-col-reverse">
             <input class="w-full" type="text" placeholder="Enter Email or Username" required="" v-model="user.email"/>
