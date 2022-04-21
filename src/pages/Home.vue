ba23d08fc4341faf46415e37783b28bdfbd4ab5d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-21 02:20:26 +0100

    changes

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,28 +5,27 @@
         <div class="logo">
           <img src="../../public/logo.png" alt="">
         </div>
-        <div class="nav flex gap-4">
+        <div class="nav flex sm:gap-1 md:gap-4 ">
           <button class="btn uppercase "><router-link to="/login">Log in</router-link></button>
           <button class="btn uppercase active-link"><router-link to="/register">sign up</router-link></button>
-          
         </div>
       </div>
     </header>
 
     <section class="mt-16">
-      <transition name="modal">
+      
         <div class="image flex justify-center items-center">
-          <img src="../../public/Group8.png" alt="">
+          <img src="../../public/Group8.png" class="sm:w-64" alt="">
         </div>
-      </transition>
+      
         <div class="title mt-12 mb-20">
           <h1 class="title1 mb-4">Fundall Expense Tracker</h1>
-          <h2>Mini Project Frontend</h2>
+          <h2 class="sm:text-[35px]">Mini Project Frontend</h2>
         </div>
         
         <div class="author">
           <transition name="modal">
-            <p>by Babatunde Fashola</p>  
+            <p class="sm:text-[20px]">by Babatunde Fashola</p>  
           </transition>
             
         </div>
@@ -47,14 +46,31 @@ export default {
 .logo {
   margin-top: 20px;
 }
+
+@media only screen and (max-width: 480px) {
+  .logo {
+    margin-top: 0px;
+    width: 40px;
+    margin-left: 10px;
+  }
+}
 .btn {
   font-size: 18px;
   line-height: 23px;
   border: 0;
   padding: 5px 20px; 
   border-radius: 10px;
-  letter-spacing: 1px;
+  /* letter-spacing: 1px; */
   font-weight: 600;
+}
+
+@media only screen and (max-width: 480px) {
+
+  .btn {
+    font-size: 12px;
+    padding: 6px 10px;
+    line-height: 16px;
+  }
 }
 .active-link {
   background: #4CE895;
@@ -68,6 +84,12 @@ section {
 .title {
   font-size: 40px;
   line-height: 51px;
+}
+
+@media only screen and (max-width: 480px) {
+  .title {
+    font-size: 35px;
+  } 
 }
 .title1 {
   font-weight: 700;
